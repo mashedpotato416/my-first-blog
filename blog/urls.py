8c1c763d52dcc78081c4697ef5804194b164c970
Hector <hector.vergara416@gmail.com>
2025-08-15 08:28:42 +0800

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Pages in the blog app
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 ### In Development this is needed
 urlpatterns += staticfiles_urlpatterns()
